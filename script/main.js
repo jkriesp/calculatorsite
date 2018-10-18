25dd255d67cefdb2f1b945d9023af53256d4d4b3
Jens Kristian Espevik <jenskristian@gmail.com>
2018-10-18 12:04:27 +0200

    finish calculator functions

---
+++ script/main.js
@@ -0,0 +1,30 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (ope, a, b) { 
+    switch (ope) {
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+        default:
+            break;
+    }
+}
